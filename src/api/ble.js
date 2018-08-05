b19e0cf0f8ca0a082065f36009911ce3d8f36100
elzup <guild0105@gmail.com>
2018-08-05 12:13:18 +0900

    Refactor ble

--- src/api/ble.js
+++ src/api/ble.js
@@ -2,262 +2,222 @@
 
 // respect https://github.com/electricbaka/bluejelly
 
-const BlueJelly = function() {
-	this.bluetoothDevice = null
-	this.dataCharacteristic = null
-	this.hashUUID = {}
-	this.hashUUIDLast = ''
+class BlueJelly {
+	onRead: Function
 
-	//callBack
-	this.onScan = function(deviceName) {
-		console.log('onScan')
-	}
-	this.onConnectGATT = function(uuid) {
-		console.log('onConnectGATT')
-	}
-	this.onRead = function(data, uuid) {
-		console.log('onRead')
-	}
-	this.onWrite = function(uuid) {
-		console.log('onWrite')
-	}
-	this.onStartNotify = function(uuid) {
-		console.log('onStartNotify')
-	}
-	this.onStopNotify = function(uuid) {
-		console.log('onStopNotify')
-	}
-	this.onDisconnect = function() {
-		console.log('onDisconnect')
-	}
-	this.onClear = function() {
-		console.log('onClear')
-	}
-	this.onReset = function() {
-		console.log('onReset')
-	}
-	this.onError = function(error) {
-		console.log('onError')
-	}
-}
-
-//--------------------------------------------------
-//setUUID
-//--------------------------------------------------
-BlueJelly.prototype.setUUID = function(name, serviceUUID, characteristicUUID) {
-	console.log('Execute : setUUID')
-	console.log(this.hashUUID)
-
-	this.hashUUID[name] = {
-		serviceUUID: serviceUUID,
-		characteristicUUID: characteristicUUID,
-	}
-}
-
-//--------------------------------------------------
-//scan
-//--------------------------------------------------
-BlueJelly.prototype.scan = function(uuid) {
-	return (this.bluetoothDevice
-		? Promise.resolve()
-		: this.requestDevice(uuid)
-	).catch(error => {
-		console.log('Error : ' + error)
-		this.onError(error)
-	})
-}
-
-//--------------------------------------------------
-//requestDevice
-//--------------------------------------------------
-BlueJelly.prototype.requestDevice = function(uuid) {
-	console.log('Execute : requestDevice')
-	return window.navigator.bluetooth
-		.requestDevice({
-			acceptAllDevices: true,
-			optionalServices: [this.hashUUID[uuid].serviceUUID],
-		})
-		.then(device => {
-			this.bluetoothDevice = device
-			this.bluetoothDevice.addEventListener(
-				'gattserverdisconnected',
-				this.onDisconnect,
-			)
-			this.onScan(this.bluetoothDevice.name)
-		})
-}
-
-//--------------------------------------------------
-//connectGATT
-//--------------------------------------------------
-BlueJelly.prototype.connectGATT = function(uuid) {
-	if (!this.bluetoothDevice) {
-		var error = 'No Bluetooth Device'
-		console.log('Error : ' + error)
-		this.onError(error)
-		return
-	}
-	if (this.bluetoothDevice.gatt.connected && this.dataCharacteristic) {
-		if (this.hashUUIDLast === uuid) return Promise.resolve()
-	}
-	this.hashUUIDLast = uuid
-
-	console.log('Execute : connect')
-	return this.bluetoothDevice.gatt
-		.connect()
-		.then(server => {
-			console.log('Execute : getPrimaryService')
-			return server.getPrimaryService(this.hashUUID[uuid].serviceUUID)
-		})
-		.then(service => {
-			console.log('Execute : getCharacteristic')
-			return service.getCharacteristic(this.hashUUID[uuid].characteristicUUID)
-		})
-		.then(characteristic => {
-			this.dataCharacteristic = characteristic
-			this.dataCharacteristic.addEventListener(
-				'characteristicvaluechanged',
-				this.dataChanged(this, uuid),
-			)
-			this.onConnectGATT(uuid)
-		})
-		.catch(error => {
-			console.log('Error : ' + error)
-			this.onError(error)
-		})
-}
-
-//--------------------------------------------------
-//dataChanged
-//--------------------------------------------------
-BlueJelly.prototype.dataChanged = function(self, uuid) {
-	return function(event) {
-		self.onRead(event.target.value, uuid)
-	}
-}
-
-//--------------------------------------------------
-//read
-//--------------------------------------------------
-BlueJelly.prototype.read = function(uuid) {
-	return this.scan(uuid)
-		.then(() => {
-			return this.connectGATT(uuid)
-		})
-		.then(() => {
-			console.log('Execute : readValue')
-			return this.dataCharacteristic.readValue()
-		})
-		.catch(error => {
-			console.log('Error : ' + error)
-			this.onError(error)
-		})
-}
-
-//--------------------------------------------------
-//write
-//--------------------------------------------------
-BlueJelly.prototype.write = function(uuid, array_value) {
-	return this.scan(uuid)
-		.then(() => {
-			return this.connectGATT(uuid)
-		})
-		.then(() => {
-			console.log('Execute : writeValue')
-			const data = Uint8Array.from(array_value)
-			return this.dataCharacteristic.writeValue(data)
-		})
-		.then(() => {
-			this.onWrite(uuid)
-		})
-		.catch(error => {
-			console.log('Error : ' + error)
-			this.onError(error)
-		})
-}
-
-//--------------------------------------------------
-//startNotify
-//--------------------------------------------------
-BlueJelly.prototype.startNotify = function(uuid) {
-	return this.scan(uuid)
-		.then(() => {
-			return this.connectGATT(uuid)
-		})
-		.then(() => {
-			console.log('Execute : startNotifications')
-			this.dataCharacteristic.startNotifications()
-		})
-		.then(() => {
-			this.onStartNotify(uuid)
-		})
-		.catch(error => {
-			console.log('Error : ' + error)
-			this.onError(error)
-		})
-}
-
-//--------------------------------------------------
-//stopNotify
-//--------------------------------------------------
-BlueJelly.prototype.stopNotify = function(uuid) {
-	return this.scan(uuid)
-		.then(() => {
-			return this.connectGATT(uuid)
-		})
-		.then(() => {
-			console.log('Execute : stopNotifications')
-			this.dataCharacteristic.stopNotifications()
-		})
-		.then(() => {
-			this.onStopNotify(uuid)
-		})
-		.catch(error => {
-			console.log('Error : ' + error)
-			this.onError(error)
-		})
-}
-
-//--------------------------------------------------
-//disconnect
-//--------------------------------------------------
-BlueJelly.prototype.disconnect = function() {
-	if (!this.bluetoothDevice) {
-		var error = 'No Bluetooth Device'
-		console.log('Error : ' + error)
-		this.onError(error)
-		return
+	bluetoothDevice: any
+	dataCharacteristic: any
+	hashUUID: any
+	hashUUIDLast: any
+	callbacks: {
+		onScan: (deviceName: string) => void,
+		onConnectGATT: (uuid: string) => void,
+		onWrite: (uuid: string) => void,
+		onStartNotify: (uuid: string) => void,
+		onStopNotify: (uuid: string) => void,
+		onDisconnect: () => void,
+		onClear: () => void,
+		onReset: () => void,
+		onError: (error: any) => void,
 	}
 
-	if (this.bluetoothDevice.gatt.connected) {
-		console.log('Execute : disconnect')
-		this.bluetoothDevice.gatt.disconnect()
-	} else {
-		const error = 'Bluetooth Device is already disconnected'
-		console.log('Error : ' + error)
-		this.onError(error)
-		return
+	constructor({ onRead }: { onRead: Function }) {
+		this.onRead = onRead
+
+		this.bluetoothDevice = null
+		this.dataCharacteristic = null
+		this.hashUUID = {}
+		this.hashUUIDLast = ''
+
+		//callBack
+		this.callbacks = {
+			...{
+				onScan: deviceName => {
+					console.log('onScan')
+				},
+				onConnectGATT: uuid => {
+					console.log('onConnectGATT')
+				},
+				onWrite: uuid => {
+					console.log('onWrite')
+				},
+				onStartNotify: uuid => {
+					console.log('onStartNotify')
+				},
+				onStopNotify: uuid => {
+					console.log('onStopNotify')
+				},
+				onDisconnect: () => {
+					console.log('onDisconnect')
+				},
+				onClear: () => {
+					console.log('onClear')
+				},
+				onReset: () => {
+					console.log('onReset')
+				},
+				onError: error => {
+					console.log('onError')
+				},
+			},
+		}
 	}
-}
+	setUUID(name: string, serviceUUID: string, characteristicUUID: string) {
+		this.hashUUID[name] = {
+			serviceUUID: serviceUUID,
+			characteristicUUID: characteristicUUID,
+		}
+	}
+	scan(uuid: string) {
+		return (this.bluetoothDevice
+			? Promise.resolve()
+			: this.requestDevice(uuid)
+		).catch(error => {
+			this.callbacks.onError(error)
+		})
+	}
+	requestDevice(uuid: string) {
+		return window.navigator.bluetooth
+			.requestDevice({
+				acceptAllDevices: true,
+				optionalServices: [this.hashUUID[uuid].serviceUUID],
+			})
+			.then(device => {
+				this.bluetoothDevice = device
+				this.bluetoothDevice.addEventListener(
+					'gattserverdisconnected',
+					this.callbacks.onDisconnect,
+				)
+				this.callbacks.onScan(this.bluetoothDevice.name)
+			})
+	}
+	connectGATT(uuid: string) {
+		if (!this.bluetoothDevice) {
+			var error = 'No Bluetooth Device'
+			this.callbacks.onError(error)
+			return
+		}
+		if (this.bluetoothDevice.gatt.connected && this.dataCharacteristic) {
+			if (this.hashUUIDLast === uuid) return Promise.resolve()
+		}
+		this.hashUUIDLast = uuid
 
-//--------------------------------------------------
-//clear
-//--------------------------------------------------
-BlueJelly.prototype.clear = function() {
-	console.log('Excute : Clear Device and Characteristic')
-	this.bluetoothDevice = null
-	this.dataCharacteristic = null
-	this.onClear()
-}
+		return this.bluetoothDevice.gatt
+			.connect()
+			.then(server => {
+				return server.getPrimaryService(this.hashUUID[uuid].serviceUUID)
+			})
+			.then(service => {
+				return service.getCharacteristic(this.hashUUID[uuid].characteristicUUID)
+			})
+			.then(characteristic => {
+				this.dataCharacteristic = characteristic
+				this.dataCharacteristic.addEventListener(
+					'characteristicvaluechanged',
+					this.dataChanged(this, uuid),
+				)
+				this.callbacks.onConnectGATT(uuid)
+			})
+			.catch(error => {
+				this.callbacks.onError(error)
+			})
+	}
 
-//--------------------------------------------------
-//reset(disconnect & clear)
-//--------------------------------------------------
-BlueJelly.prototype.reset = function() {
-	console.log('Excute : reset')
-	this.disconnect() //disconnect() is not Promise Object
-	this.clear()
-	this.onReset()
+	dataChanged(self: BlueJelly, uuid: string) {
+		return event => {
+			self.onRead(event.target.value, uuid)
+		}
+	}
+
+	read(uuid: string) {
+		return this.scan(uuid)
+			.then(() => {
+				return this.connectGATT(uuid)
+			})
+			.then(() => {
+				return this.dataCharacteristic.readValue()
+			})
+			.catch(error => {
+				this.callbacks.onError(error)
+			})
+	}
+
+	write(uuid: string, array: any[]) {
+		return this.scan(uuid)
+			.then(() => {
+				return this.connectGATT(uuid)
+			})
+			.then(() => {
+				const data = Uint8Array.from(array)
+				return this.dataCharacteristic.writeValue(data)
+			})
+			.then(() => {
+				this.callbacks.onWrite(uuid)
+			})
+			.catch(error => {
+				this.callbacks.onError(error)
+			})
+	}
+
+	startNotify(uuid: string) {
+		return this.scan(uuid)
+			.then(() => {
+				return this.connectGATT(uuid)
+			})
+			.then(() => {
+				this.dataCharacteristic.startNotifications()
+			})
+			.then(() => {
+				this.callbacks.onStartNotify(uuid)
+			})
+			.catch(error => {
+				this.callbacks.onError(error)
+			})
+	}
+
+	stopNotify(uuid: string) {
+		return this.scan(uuid)
+			.then(() => {
+				return this.connectGATT(uuid)
+			})
+			.then(() => {
+				this.dataCharacteristic.stopNotifications()
+			})
+			.then(() => {
+				this.callbacks.onStopNotify(uuid)
+			})
+			.catch(error => {
+				this.callbacks.onError(error)
+			})
+	}
+
+	disconnect() {
+		if (!this.bluetoothDevice) {
+			var error = 'No Bluetooth Device'
+			this.callbacks.onError(error)
+			return
+		}
+
+		if (this.bluetoothDevice.gatt.connected) {
+			this.bluetoothDevice.gatt.disconnect()
+		} else {
+			const error = 'Bluetooth Device is already disconnected'
+			this.callbacks.onError(error)
+			return
+		}
+	}
+
+	clear() {
+		this.bluetoothDevice = null
+		this.dataCharacteristic = null
+		this.callbacks.onClear()
+	}
+
+	async reset() {
+		await this.disconnect() //disconnect() is not Promise Object
+		this.clear()
+		this.callbacks.onReset()
+	}
 }
 
 export default BlueJelly
