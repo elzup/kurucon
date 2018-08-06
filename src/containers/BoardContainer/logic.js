1c97e38641f362c1a0c49af298e9e4401d36ceca
elzup <guild0105@gmail.com>
2018-08-06 14:13:02 +0900

    Wall

--- src/containers/BoardContainer/logic.js
+++ src/containers/BoardContainer/logic.js
@@ -6,27 +6,31 @@ import * as actions from './actions'
 import * as selectors from './selectors'
 import { updateCell } from '../CellById/actions'
 
+const angle = (pit: number, yaw: number): 'r' | 'l' | 'u' | 'd' => {
+	if (pit === 1) {
+		return 'r'
+	}
+	if (pit === -1) {
+		return 'l'
+	}
+	if (yaw === 1) {
+		return 'd'
+	}
+	return 'u'
+}
+
+const limit = (n: number) => {
+	return Math.min(Math.max(0, n), 4)
+}
+
 export function movePlayer(pit: number, yaw: number): ThunkAction {
 	return async (dispatch, getState) => {
 		const board = selectors.getBoard(getState())
 		if (pit === 0 && yaw === 0) {
 			return
 		}
-		const x = (board.player.x + pit + 5) % 5
-		const y = (board.player.y + yaw + 5) % 5
-
-		const angle = (pit: number, yaw: number): 'r' | 'l' | 'u' | 'd' => {
-			if (pit === 1) {
-				return 'r'
-			}
-			if (pit === -1) {
-				return 'l'
-			}
-			if (yaw === 1) {
-				return 'd'
-			}
-			return 'u'
-		}
+		const x = limit(board.player.x + pit)
+		const y = limit(board.player.y + yaw)
 
 		await dispatch(
 			updateCell(makeId(board.player.x, board.player.y), {
